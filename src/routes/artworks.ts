9b88619c549d9bab37c083c0ae15e411bd50a06a
James Secor <jsecor@dealerpolicy.com>
2023-02-14 16:18:08 -0500

    added update artwork by id

--- src/routes/artworks.ts
+++ src/routes/artworks.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authenticateRequest } from '../models/authentication';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const artworksCollection = 'artworks';
 
@@ -60,6 +60,28 @@ export const register = (app: express.Application) => {
                 res.status(200).send({ message: "inserted successfully" });
             } else {
                 res.status(400).send({ message: 'failed to insert' });
+            }
+        } catch (err) {
+            let message = 'unknown error';
+            if (err instanceof Error) {
+                message = err.message;
+            }
+            res.status(400).send({ error: err, message });
+        }
+    });
+
+    app.put('/api/artworks/:id', async (req, res) => {
+        try {
+            const client = new MongoClient(process.env.DB_CONNECTIONSTRING ?? '');
+            await client.connect();
+            const db = client.db(process.env.DB_NAME);
+            const collection = db.collection(artworksCollection);
+
+            const update = await collection.updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body });
+            if (update) {
+                res.status(200).send({ message: "updated successfully" });
+            } else {
+                res.status(400).send({ message: 'failed to update' });
             }
         } catch (err) {
             let message = 'unknown error';
